fec05418c2ec31aea43de28f477b090d39e6e436
Wire List <114312567+Wire-List@users.noreply.github.com>
2022-09-25 20:20:11 +0200

    Update clqu.config.js

--- clqu.config.js
+++ clqu.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'Stoneclane Development',
+    name: 'Wire',
     version: '4.0.0',
-    githubName: 'Stoneclane-Development',
+    githubName: 'Wire-List',
     githubKey: process.env.GITHUB_KEY,
     softwareerKey: process.env.SOFTWAREER_KEY,
-    email: 'me@janjytapyt.me',
+    email: 'me@wire.me',
     errors: {
         404: "This page could not be found.",
         500: "An error occurred while processing your request."
