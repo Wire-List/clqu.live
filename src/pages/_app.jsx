d76c37d15f3b9e73974ba424be8088f350de286b
Wire List <114312567+Wire-List@users.noreply.github.com>
2022-09-25 20:22:28 +0200

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
       <PageProvider>
         <main>
           <head>
-            <title>clqu.live | Full-stack Developer</title>
+            <title>wire.me | Full-stack Developer</title>
           </head>
           <CustomCursor />
           <ContextMenu
